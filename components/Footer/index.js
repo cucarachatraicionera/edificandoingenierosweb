d9f297aa29f289a91cafb81a2705348dc2378d25
cucarachatraicionera <cucarachatraicionera@gmail.com>
2025-04-28 14:35:10 -0500

    agregando telefono

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -16,7 +16,9 @@ const Footer = ({}) => {
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
               JUNTOS
             </h1>
-            <Button type="primary">Llamanos</Button>
+            <a href="tel:+573117720385">
+              <Button type="primary">Llámanos</Button>
+            </a>
             <div className="mt-10">
               <Socials />
             </div>
