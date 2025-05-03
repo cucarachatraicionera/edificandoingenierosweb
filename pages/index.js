d6a72f9892358a741081c61073ad41b5a4440bb1
cucarachatraicionera <cucarachatraicionera@gmail.com>
2025-05-03 11:12:20 -0500

    Servicios. en engrilla

--- pages/index.js
+++ pages/index.js
@@ -110,8 +110,9 @@ export default function Home() {
         </div>
 
         <div className="mt-10 laptop:mt-30 p-2 laptop:p-0">
-          <h1 className="tablet:m-10 text-2xl text-bold">Servicios.</h1>
-          <div className="mt-5 tablet:m-10 grid grid-cols-1 laptop:grid-cols-2 gap-6">
+        <h1 className="tablet:m-10 text-3xl font-bold">Servicios.</h1>
+
+        <div className="mt-5 tablet:m-10 grid grid-cols-1 laptop:grid-cols-2 gap-6">
             {data.services.map((service, index) => (
               <ServiceCard
                 key={index}
